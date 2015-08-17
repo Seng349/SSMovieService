fc4a432924c4a3bb39260a68a287e71714a179e5
ZhengWenQiang <ZhengWenQiang@SengdeMac-mini.local>
2015-08-17 17:06:57 +0800

    GUI update

--- GUIService/TkinterService.py
+++ GUIService/TkinterService.py
@@ -1,4 +1,5 @@
 import Tkinter;
+from Tkinter import *;
 
 class TkinterMainInterface(object):
 	mInterface = 0;
@@ -8,6 +9,13 @@ class TkinterMainInterface(object):
 		super(TkinterMainInterface, self).__init__()
 		self.mInterface = Tkinter.Tk();
 		self.mInterface.title("SSMovieService")
+		self.mInterface.geometry('800x600');
+
+		# First Line
+		aFirstLineFrame = Frame(self.mInterface);
+		aFirstLineLabel = Label(aFirstLineFrame, text = "The Movie File: ", height = 60)
+		aFirstLineLabel.pack();
+		aFirstLineFrame.pack(fill = BOTH)
 
 	def show(self):
 		self.mInterface.mainloop();
