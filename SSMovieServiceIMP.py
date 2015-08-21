9fdfc21c6c4f8ce07196ad75b3e396b14f38bf00
ZhengWenQiang <ZhengWenQiang@SengdeMac-mini.local>
2015-08-21 18:04:26 +0800

    update project

--- SSMovieServiceIMP.py
+++ SSMovieServiceIMP.py
@@ -9,29 +9,47 @@ import os;
 import string;
 
 def SplitMoviesStart(aOriginalMoviePath, aStoreMoviePath, aSplitMoviesCount, aMovieType, aGithubFilePath, aGithubCommitMessage):
-	# if not os.path.exists(aOriginalMoviePath):
-	# 	aInterface.showAlertView("The Original Movie Path can't be empty !!!")
-	# 	return;
+	if not os.path.exists(aOriginalMoviePath):
+		aInterface.showAlertView("The Original Movie Path can't be empty !!!")
+		return;
 
-	# if not aStoreMoviePath:
-	# 	aInterface.showAlertView("The Store Movie Path can't be empty !!!")
-	# 	return;
+	if not aStoreMoviePath:
+		aInterface.showAlertView("The Store Movie Path can't be empty !!!")
+		return;
 
-	# if aSplitMoviesCount <= 0:
-	# 	aInterface.showAlertView("The Split Movies Count can't be negative !!!");
-	# 	return;
+	if aSplitMoviesCount <= 0:
+		aInterface.showAlertView("The Split Movies Count can't be negative !!!");
+		return;
 
-	# if not os.path.exists(aOriginalMoviePath):
-	# 	aInterface.showAlertView("The Original Movie Path not exit !!!");
-	# 	return;
+	if not os.path.exists(aOriginalMoviePath):
+		aInterface.showAlertView("The Original Movie Path not exit !!!");
+		return;
 
-	# if not os.path.exists(aStoreMoviePath):
-	# 	os.makedirs(aStoreMoviePath);
-	# 	pass
+	if not os.path.exists(aStoreMoviePath):
+		os.makedirs(aStoreMoviePath);
+		pass
 
+
+	for aFileName in os.listdir(aOriginalMoviePath):
+		aFilePath = ''
+		if cmp(aOriginalMoviePath[len(aOriginalMoviePath) - 1:], '/')
+			aFilePath = aOriginalMoviePath + aFileName;
+			pass
+		else:
+			aFilePath = aOriginalMoviePath + '/' + aFileName;
+			pass;
+
+		aMovieEditor = SSMovieEditor(aFilePath);
+
+		if aMovieEditor.GetIsReadSuccess():
+			aMovieEditor.SplitMoviesToFile(aSplitMoviesCount, aMovieType, aStoreMoviePath)
+			pass
+		pass
 	
 	if len(aGithubFilePath) > 0:
-		print 'updating'
+		if len(aGithubCommitMessage) == 0:
+			aInterface.showAlertView("The Github Commit Message can't be empty")
+			return
 		aGithubService = GithubUploadService(aGithubFilePath);
 		aGithubService.upload(aGithubCommitMessage)
 		pass
